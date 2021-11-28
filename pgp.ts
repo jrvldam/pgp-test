285e1e6fb0b7051121c89210e0c1453ca78edfc7
jrvldam <jrvldam@gmail.com>
2021-11-28 16:57:58 +0100

    code formating

--- pgp.ts
+++ pgp.ts
@@ -1,16 +1,54 @@
-import { EllipticCurveName, decrypt, generateKey, readPrivateKey, decryptKey, encrypt, readKey, createMessage, readMessage, Message, VerificationResult } from 'openpgp';
+import {
+  EllipticCurveName,
+  decrypt,
+  generateKey,
+  readPrivateKey,
+  decryptKey,
+  encrypt,
+  readKey,
+  createMessage,
+  readMessage,
+  Message,
+  VerificationResult,
+} from 'openpgp';
 
-export async function generateKeys({ userIds, curve, passphrase }: { userIds: { name?: string; email?: string }[]; curve?: EllipticCurveName; passphrase?: string }): Promise<{ privateArmoredKey: string; publicArmoredKey: string; revocationCertificate: string }> {
+export async function generateKeys({
+  userIds,
+  curve,
+  passphrase,
+}: {
+  userIds: { name?: string; email?: string }[];
+  curve?: EllipticCurveName;
+  passphrase?: string;
+}): Promise<{
+  privateArmoredKey: string;
+  publicArmoredKey: string;
+  revocationCertificate: string;
+}> {
   const { privateKey, publicKey, revocationCertificate } = await generateKey({
     userIDs: userIds,
     curve,
     passphrase,
   });
-    
-  return { privateArmoredKey: privateKey, publicArmoredKey: publicKey, revocationCertificate };
+
+  return {
+    privateArmoredKey: privateKey,
+    publicArmoredKey: publicKey,
+    revocationCertificate,
+  };
 }
 
-export async function encryptText({ text, publicArmoredKey, privateArmoredKey, passphrase }: { text: string; publicArmoredKey: string; privateArmoredKey: string; passphrase: string }): Promise<string> {
+export async function encryptText({
+  text,
+  publicArmoredKey,
+  privateArmoredKey,
+  passphrase,
+}: {
+  text: string;
+  publicArmoredKey: string;
+  privateArmoredKey: string;
+  passphrase: string;
+}): Promise<string> {
   const publicKey = await readKey({ armoredKey: publicArmoredKey });
   const privateKey = await decryptKey({
     privateKey: await readPrivateKey({ armoredKey: privateArmoredKey }),
@@ -30,8 +68,18 @@ export async function getMessage(token: string) {
   return readMessage({ armoredMessage: token });
 }
 
-export async function decryptText({ message, publicArmoredKey, privateArmoredKey, passphrase }: { message: Message<string>; publicArmoredKey: string; privateArmoredKey: string; passphrase: string }): Promise<{ decrypted: string; signatures: VerificationResult[] }> {
-  const publicKey = await readKey({ armoredKey: publicArmoredKey }); 
+export async function decryptText({
+  message,
+  publicArmoredKey,
+  privateArmoredKey,
+  passphrase,
+}: {
+  message: Message<string>;
+  publicArmoredKey: string;
+  privateArmoredKey: string;
+  passphrase: string;
+}): Promise<{ decrypted: string; signatures: VerificationResult[] }> {
+  const publicKey = await readKey({ armoredKey: publicArmoredKey });
   const privateKey = await decryptKey({
     privateKey: await readPrivateKey({ armoredKey: privateArmoredKey }),
     passphrase,
@@ -41,11 +89,13 @@ export async function decryptText({ message, publicArmoredKey, privateArmoredKey
     decryptionKeys: privateKey,
     verificationKeys: publicKey, // Optional
   });
-  
+
   return { decrypted: data, signatures };
 }
 
-export async function verfySignatures(signatures: VerificationResult[]): Promise<boolean> {
+export async function verfySignatures(
+  signatures: VerificationResult[],
+): Promise<boolean> {
   try {
     await signatures[0].verified;
     return true;
